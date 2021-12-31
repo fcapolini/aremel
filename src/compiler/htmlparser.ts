6593f19da9a6f93fdaef8414c946790f1d7e3bcb
fcapolini <fabrizio@ubimate.com>
2021-12-31 19:52:03 +0100

    htmlparser

--- src/compiler/htmlparser.ts
+++ src/compiler/htmlparser.ts
@@ -19,20 +19,20 @@ export default class HtmlParser {
 		var origin = this.origins.length;
 		this.origins.push(fname);
 		var ret = new HtmlDocument(origin);
-		var i = this.parseNodes(ret, s, 0, origin);
+		var i = this._parseNodes(ret, s, 0, origin);
 		if (i < s.length) {
 			new HtmlText(ret, ret, s.substr(i), i, s.length, origin);
 		}
 		return ret;
 	}
 
-	parseNodes(p:HtmlElement, s:string, i1:number, origin:number) {
+	_parseNodes(p:HtmlElement, s:string, i1:number, origin:number) {
 		var i2, closure, i3 = i1, i4, closetag = null;
 		while ((i2 = s.indexOf('<', i1)) >= 0) {
 			i4 = i2;
 			i1 = i2 + 1;
 			(closure = s.charCodeAt(i1) === '/'.charCodeAt(0)) ? i1++ : null;
-			if ((i2 = this.skipName(s, i1)) > i1) {
+			if ((i2 = this._skipName(s, i1)) > i1) {
 				if (i4 > i3) {
 					new HtmlText(p.ownerDocument, p, s.substring(i3, i4),
 								i3, i4, origin, false);
@@ -58,10 +58,10 @@ export default class HtmlParser {
 					}
 					i1 = i2;
 				} else {
-					i1 = this.parseElement(p, s, i1, i2, origin);
+					i1 = this._parseElement(p, s, i1, i2, origin);
 				}
 				i3 = i1;
-			} else if (!closure && (i2 = this.skipComment(s, i1, origin)) > i1) {
+			} else if (!closure && (i2 = this._skipComment(s, i1, origin)) > i1) {
 				if (i4 > i3) {
 					new HtmlText(p.ownerDocument, p, s.substring(i3, i4),
 								i3, i4, origin, false);
@@ -82,11 +82,11 @@ export default class HtmlParser {
 		return i1;
 	}
 
-	parseElement(p:HtmlElement, s:string,
+	_parseElement(p:HtmlElement, s:string,
 				i1:number, i2:number, origin:number): number {
 		var e = new HtmlElement(p.ownerDocument, p, s.substring(i1, i2), i1, i2, origin);
-		i1 = this.parseAttributes(e, s, i2, origin);
-		i1 = this.skipBlanks(s, i1);
+		i1 = this._parseAttributes(e, s, i2, origin);
+		i1 = this._skipBlanks(s, i1);
 		var selfclose = false;
 		if ((selfclose = (s.charCodeAt(i1) === '/'.charCodeAt(0)))) {
 			i1++;
@@ -100,7 +100,7 @@ export default class HtmlParser {
 		i1++;
 		if (!selfclose && !VOID_ELEMENTS.has(e.tagName)) {
 			if (SKIP_CONTENT_TAGS.has(e.tagName)) {
-				var res = this.skipContent(e.tagName, s, i1, origin);
+				var res = this._skipContent(e.tagName, s, i1, origin);
 				if (!res) {
 					throw new HtmlException(
 						`Unterminated tag ${e.tagName}`,
@@ -113,27 +113,27 @@ export default class HtmlParser {
 				}
 				i1 = res.i2;
 			} else {
-				i1 = this.parseNodes(e, s, i1, origin);
+				i1 = this._parseNodes(e, s, i1, origin);
 			}
 		}
 		return i1;
 	}
 
-	parseAttributes(e:HtmlElement, s:string, i2:number, origin:number) {
-		var i1 = this.skipBlanks(s, i2);
-		while ((i2 = this.skipName(s, i1, true)) > i1) {
+	_parseAttributes(e:HtmlElement, s:string, i2:number, origin:number) {
+		var i1 = this._skipBlanks(s, i2);
+		while ((i2 = this._skipName(s, i1, true)) > i1) {
 			//TODO: ignore `::<attributes>`, used for comments
 			var name = s.substring(i1, i2);
 			var a = e.setAttribute(name, '', undefined, i1, i2, origin);
-			i1 = this.skipBlanks(s, i2);
+			i1 = this._skipBlanks(s, i2);
 			if (s.charCodeAt(i1) === '='.charCodeAt(0)) {
-				i1 = this.skipBlanks(s, i1 + 1);
+				i1 = this._skipBlanks(s, i1 + 1);
 				var quote = s.charCodeAt(i1);
 				if (a && (quote === '"'.charCodeAt(0) || quote === "'".charCodeAt(0))) {
-					i1 = this.parseValue(a, s, i1 + 1, quote, String.fromCharCode(quote), origin);
+					i1 = this._parseValue(a, s, i1 + 1, quote, String.fromCharCode(quote), origin);
 			// #if (HTML_EXTENSIONS)
 				} else if (a && (quote === '['.charCodeAt(0) && s.charCodeAt(i1 + 1) === '['.charCodeAt(0))) {
-					i1 = this.parseValue(a, s, i1 + 2, quote, ']]', origin);
+					i1 = this._parseValue(a, s, i1 + 2, quote, ']]', origin);
 			// #end
 				} else {
 					// we don't support unquoted attribute values
@@ -142,12 +142,12 @@ export default class HtmlParser {
 					);
 				}
 			}
-			i1 = this.skipBlanks(s, i1);
+			i1 = this._skipBlanks(s, i1);
 		};
 		return i1;
 	}
 
-	parseValue(a:HtmlAttribute, s:string, i1:number,
+	_parseValue(a:HtmlAttribute, s:string, i1:number,
 						quote:number, term:string, origin:number) {
 		var i2 = s.indexOf(term, i1);
 		if (i2 < 0) {
@@ -168,7 +168,7 @@ export default class HtmlParser {
 		return i1;
 	}
 
-	skipComment(s:string, i1:number, origin:number) {
+	_skipComment(s:string, i1:number, origin:number) {
 		if (s.charCodeAt(i1) === '!'.charCodeAt(0)
 			&& s.charCodeAt(i1 + 1) === '-'.charCodeAt(0)
 			&& s.charCodeAt(i1 + 2) === '-'.charCodeAt(0)) {
@@ -183,15 +183,15 @@ export default class HtmlParser {
 		return i1;
 	}
 
-	skipContent(tag:string, s:string, i1:number, origin:number) {
+	_skipContent(tag:string, s:string, i1:number, origin:number) {
 		var i2;
 		while ((i2 = s.indexOf('</', i1)) >= 0) {
 			var i0 = i2;
 			i1 = i2 + 2;
-			i2 = this.skipName(s, i1);
+			i2 = this._skipName(s, i1);
 			if (i2 > i1) {
 				if (s.substring(i1, i2).toUpperCase() === tag) {
-					i2 = this.skipBlanks(s, i2);
+					i2 = this._skipBlanks(s, i2);
 					if (s.charCodeAt(i2) != '>'.charCodeAt(0)) {
 						throw new HtmlException(
 							'Unterminated close tag',
@@ -208,7 +208,7 @@ export default class HtmlParser {
 		return null;
 	}
 
-	skipBlanks(s:string, i:number) {
+	_skipBlanks(s:string, i:number) {
 		while (i < s.length) {
 			if (s.charCodeAt(i) > 32) {
 				break;
@@ -218,7 +218,7 @@ export default class HtmlParser {
 		return i;
 	}
 
-	skipName(s:string, i:number, acceptsDots=false) {
+	_skipName(s:string, i:number, acceptsDots=false) {
 		while (i < s.length) {
 			var code = s.charCodeAt(i);
 			if ((code < 'a'.charCodeAt(0) || code > 'z'.charCodeAt(0)) &&
